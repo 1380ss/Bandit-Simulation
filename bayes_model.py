9f76b43078997601d567a59aae3f9e1456d10f7f
1380ss <to.li@mail.utoronto.ca>
2024-09-26 08:29:44 -0400

    added some comment in code

--- bayes_model.py
+++ bayes_model.py
@@ -1,6 +1,13 @@
+#this file contains all Bayes updating models.
+#in the future can check if there's packages available
+
 import numpy as np
 
 class beta_bernoulli:
+    """
+    Beta-Bernoulli model
+    """
+
     def __init__(self, number_of_arms, prior=None):
         self.number_of_arms = number_of_arms
         if prior is None:
@@ -12,17 +19,29 @@ class beta_bernoulli:
                           'b': None}
 
     def get_posterior_sample(self, size=1):
+        """
+        get (multiple) posterior sample using current 'self.posterior'
+        :param size: number of posterior samples
+        :return:
+        """
 
         return (np.random.beta(**self.posterior, size=(size,) + self.posterior['a'].shape))
 
     def update_posterior(self, action_hist, reward_hist):
+        """
+        update posterior distribution based on reward and action history.
+        This needs to be called at the beginning to initialize posterior
+        :param action_hist:
+        :param reward_hist:
+        :return:
+        """
 
-        B = action_hist.shape[0]
+        b = action_hist.shape[0]
         reward_counts = np.transpose(np.array(
             [np.sum(reward_hist * (action_hist == i), axis=1) for i in range(self.number_of_arms)]
         ))
         action_counts = np.transpose(np.array(
             [np.sum((action_hist == i), axis=1) for i in range(self.number_of_arms)]
         ))
-        self.posterior['a'] = np.tile(self.prior['a'], (B, 1)) + reward_counts
-        self.posterior['b'] = np.tile(self.prior['b'], (B, 1)) + action_counts - reward_counts
+        self.posterior['a'] = np.tile(self.prior['a'], (b, 1)) + reward_counts
+        self.posterior['b'] = np.tile(self.prior['b'], (b, 1)) + action_counts - reward_counts
